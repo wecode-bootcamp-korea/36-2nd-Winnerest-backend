dc6324e6f59730900b54b9f3fa471a76fe942f00
Gil Seongmin(Mark) <ksm65501@gmail.com>
2022-08-29 18:06:21 +0900

    PROJECT 초기세팅
    * PROJECT 초기세팅
    
    * [EIDT] 초기 세팅 수정

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/ping', (req, res)=> {
+    res.json({message : 'pong'});
+})
+
+app.listen(PORT, ()=> {
+    console.log(`server listening on PORT ${PORT}`);
+}
+);
