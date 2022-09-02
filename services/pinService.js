582d3a95105a29add23055061fcbde6e4d4ac0b7
a <a@aui-MacBookAir.local>
2022-09-02 21:04:55 +0900

    ADD: pin 기능구현중2

--- services/pinService.js
+++ services/pinService.js
@@ -1,11 +1,11 @@
 const pinDao = require('../models/pinDao')
-const error = require("../middlewares/errorCreater");
+const ErrorCreater = require("../middlewares/errorCreater");
 
 const createPin = async (boardId, title, contents, tagId, imgUrl) => {
     
     const Tag = await pinDao.getMyPinTag(tagId)
     
-    if(!Tag){throw new error("INVAILD TAG", 404)}
+    if(!Tag){throw new ErrorCreater("INVAILD TAG", 404)}
     
     const createMyPin = await pinDao.createMyPin(boardId, title, contents, tagId, imgUrl);
     return createMyPin}
