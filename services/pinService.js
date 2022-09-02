f08b4b9f96bea4e4698876247ac0e16f21cc4bc4
a <a@aui-MacBookAir.local>
2022-09-02 21:28:11 +0900

    ADD: pin 기능구현중3

--- services/pinService.js
+++ services/pinService.js
@@ -3,9 +3,9 @@ const ErrorCreater = require("../middlewares/errorCreater");
 
 const createPin = async (boardId, title, contents, tagId, imgUrl) => {
     
-    const Tag = await pinDao.getMyPinTag(tagId)
+    //const Tag = await pinDao.getMyPinTag(tagId)
     
-    if(!Tag){throw new ErrorCreater("INVAILD TAG", 404)}
+    //if(!Tag){throw new ErrorCreater("INVAILD TAG", 404)}
     
     const createMyPin = await pinDao.createMyPin(boardId, title, contents, tagId, imgUrl);
     return createMyPin}
