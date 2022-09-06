dbb0476dd0d8bca99a44831902d775f5b75354ae
길길성민 <ksm65501@gmail.com>
2022-09-06 18:50:44 +0900

    [ADD]추천 Pin 조회 API End-poin 구현
    [EDIT] pagination 기능 코드 수정

--- routes/pinRouter.js
+++ routes/pinRouter.js
@@ -5,6 +5,7 @@ const auth = require('../middlewares/auth');
 const pinRouter = express.Router();
 
 pinRouter.get('/', errorHandler(auth.validationToken), errorHandler(pinController.findMainPins));
+pinRouter.get('/recommend/:tagId', errorHandler(auth.validationToken), errorHandler(pinController.findRecommendPins));
 pinRouter.get('/:pinId', pinController.getPinInfo);
 
 module.exports = {
