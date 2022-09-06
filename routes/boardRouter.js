ab23471b08c690e2a291d5b0474d0baeeaf8aacd
길길성민 <ksm65501@gmail.com>
2022-09-06 16:38:04 +0900

    [ADD] BOARD CRUD API 기능 END-POINT 구현

--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -5,7 +5,12 @@ const errorHandler = require('../middlewares/errorHandler')
 const auth = require('../middlewares/auth');
 const boardController = require('../controllers/boardController');
 
+boardRouter.get('/title', errorHandler(auth.validationToken), errorHandler(boardController.getBoardTitle));
+boardRouter.get('/userId/:userId', errorHandler(auth.validationToken), errorHandler(boardController.getUserBoard));
+boardRouter.get('/:boardId', errorHandler(auth.validationToken), errorHandler(boardController.getBoardInfo));
 boardRouter.post('/', errorHandler(auth.validationToken), errorHandler(boardController.createBoard));
+boardRouter.patch('/:boardId', errorHandler(auth.validationToken), errorHandler(boardController.updateBoard));
+boardRouter.delete('/:boardId', errorHandler(auth.validationToken), errorHandler(boardController.deleteBoard));
 
 module.exports = {
     boardRouter
